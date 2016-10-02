eb89a182f6d78335a3ce14b4a066d3f8802e0c9c
Nick Phillips <phillipn101@gmail.com>
2016-10-02 13:34:35 -0500

    heroku error

--- app_client/app.js
+++ app_client/app.js
@@ -9,32 +9,32 @@
         controller: 'homeCtrl',
         controllerAs: "vm"
       })
-      .when('/book/:bookid', {
-        templateUrl: "/book/book.view.html",
-        controller: 'bookCtrl',
-        controllerAs: "vm"
-      })
-      .when('/login', {
-        templateUrl: "/auth/login/login.view.html",
-        controller: 'loginCtrl',
-        controllerAs: "vm"
-      })
-      .when('/register', {
-        templateUrl: "/auth/register/register.view.html",
-        controller: 'registerCtrl',
-        controllerAs: "vm"
-      })
-      .when('/profile', {
-        templateUrl: "/auth/profile/profile.view.html",
-        controller: 'profileCtrl',
-        controllerAs: "vm"
-      })
-      .when('/user/:userid', {
-        templateUrl: "/auth/trades/trades.view.html",
-        controller: 'tradesCtrl',
-        controllerAs: "vm"
-      })
-      .otherwise({redirectTo: '/'});
+      // .when('/book/:bookid', {
+      //   templateUrl: "/book/book.view.html",
+      //   controller: 'bookCtrl',
+      //   controllerAs: "vm"
+      // })
+      // .when('/login', {
+      //   templateUrl: "/auth/login/login.view.html",
+      //   controller: 'loginCtrl',
+      //   controllerAs: "vm"
+      // })
+      // .when('/register', {
+      //   templateUrl: "/auth/register/register.view.html",
+      //   controller: 'registerCtrl',
+      //   controllerAs: "vm"
+      // })
+      // .when('/profile', {
+      //   templateUrl: "/auth/profile/profile.view.html",
+      //   controller: 'profileCtrl',
+      //   controllerAs: "vm"
+      // })
+      // .when('/user/:userid', {
+      //   templateUrl: "/auth/trades/trades.view.html",
+      //   controller: 'tradesCtrl',
+      //   controllerAs: "vm"
+      // })
+      // .otherwise({redirectTo: '/'});
   }
   
   angular.module('bookstoreApp')
